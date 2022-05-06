7c76868686dbd880be01f4402105dda150867a4f
Stormpero <staralex.const@gmail.com>
2022-05-06 15:43:37 +0300

    refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Game from "./Game";
 import Background from "./background/Background";
 import DevProvider from "./usedev/DevProvider";
-
-
 
 function App() {
   useEffect(() => {
